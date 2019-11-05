34e52bdcc7f907e79e36c169a3654eea8549b83a
Brian PKM <brian.lijua@pkmgroup.com>
2019-11-05 15:37:47 +0800

    fix video logic

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -167,7 +167,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    if auto_show and is_video:
+    if auto_show and not is_video:
         plt.show()
 
     img_buffer = io.BytesIO()    
