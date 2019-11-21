924dcb92b94321533314fe00111a48479de6d017
Brian PKM <brian.lijua@pkmgroup.com>
2019-11-21 15:46:43 +0800

    Added static folder, and on AI processing success, "prediction()" will return a URL directly pointing to processes image

--- pkmai_server.py
+++ pkmai_server.py
@@ -1,28 +1,31 @@
 import os
 import uuid
-from flask import Flask, request
+from flask import Flask, request, render_template, send_from_directory
 
 from pkmai.pkmai_backbone import PKMAI_BACKBONE as serve
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='processed')
+ai = serve()
 
 TEMP_FOLDER = os.path.join(os.getcwd(), 'temp', 'server')
+PROCESSED_FOLDER = os.path.join(os.getcwd(), 'processed')
 
 @app.route('/')
 def index():
-    return "Welcome to PKM AI"
+    return render_template('index.html')
 
 @app.route('/prediction', methods=['POST'])
 def prediction():
 
-    post_img = request.files.get('image')
+    post_img = request.files.get('ai_image')
 
     unique_name = uuid.uuid4()
     temp_save_path = os.path.join(TEMP_FOLDER, '{}.jpg'.format(unique_name))
-    post_img.save(temp_save_path)          
-
-    ai = serve()
-    ai.prediction(temp_save_path)
-    return 'Done prediction'
+    post_img.save(temp_save_path)
+    ret_url = ai.prediction(temp_save_path)
+    fn = ret_url.split('\\')
+    fn = fn[len(fn) - 1]    
+    # return "{}/{}".format(request.url_root, app.send_static_file(fn))    
+    return '{}processed/{}'.format(request.url_root, fn)
 
 app.run(debug=True)
